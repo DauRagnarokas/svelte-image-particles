108376a039f861bf19ef4aa9873ae6cd76d7827e
Daugirdas Rušinas <dauart@gmail.com>
2025-09-10 00:39:29 +0300

    Removed demo component

--- src/lib/DemoBox.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  export let title = 'Hello from DemoBox';
-</script>
-
-<div style="padding:1rem;border:4px dashed currentColor;border-radius:12px;">
-  <strong>{title}</strong>
-  <div><slot /></div>
-</div>
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,2 @@
 export { default as ParticlesCanvas } from './ParticlesCanvas.svelte';
-export { default as DemoBox } from './DemoBox.svelte';
 export { default } from './ParticlesCanvas.svelte';
